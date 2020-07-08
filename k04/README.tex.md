f3c291ccd4ccf8bffc8b001b0d1cb42d1a4fabe5
bb35319011 <bb35319011@ms.nagasaki-u.ac.jp>
2020-07-08 15:05:33 +0900

    k04提出, 7/8

--- k04/README.tex.md
+++ k04/README.tex.md
@@ -1,6 +1,6 @@
 # 課題4 レポート
 
-aa83988848 薗田光太郎
+bb35319011 江島奨悟
 
 ## 課題
 
@@ -11,7 +11,23 @@ aa83988848 薗田光太郎
 この二つのファイルを読み取り，学籍番号，身長，性別の3つをメンバーとする構造体の配列で管理するとよい．
 
 ## ソースコードの説明
+- l6:学生番号、性別、身長を保持する構造体を宣言
+- l20~：
+  ファイルオープン処理.
+  fp1でheight.csv、fp2でID.csvを操作.
+- l54~:
+  ファイルから値を取り出して格納.
+  height->buf1 , IDs -> buf2.
+  格納先の構造体配列にポインタspでアクセス.
+  height.csvの読み取りが完了したら終了させる.
+- l70：検索値の入力を受け取る
+- l73~:
+  stDataに格納した学生番号と入力値を総当たり.
+  一致した場合その番号のデータを出力してプログラムを終了.
+  一致する値がない場合"no data"を出力して終了.
 
+
+  
 
 ## 入出力結果
 
@@ -35,6 +51,20 @@ Input the filename of sample ID : ../sample/IDs.csv
 Which ID's data do you want? : 45313124
 ---
 No data
+```
+
+実行結果
+```
+input the filename of height:c:\Users\bb35319011\Desktop\2020psp2\sample\heights.csv
+the filename of sample: c:\Users\bb35319011\Desktop\2020psp2\sample\heights.csv
+input the filename of IDs:c:\Users\bb35319011\Desktop\2020psp2\sample\IDs.csv
+the filename of sample: c:\Users\bb35319011\Desktop\2020psp2\sample\IDs.csv
+which ID's data do you want?:45313125
+---
+ID:45313125
+gender:female
+height:152.400
+---
 ```
 
 ## 修正履歴
--- k04/k04.c
+++ k04/k04.c
@@ -1,1 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
+//学生番号・性別・身長
+struct student{
+    int num;
+    int gen;
+    double height;
+};
+
+int main(){
+    int num;
+    int gen;
+    double height;
+    int imput;
+    int i;
+    struct student* sp;
+
+    //ファイルオープン処理===========================
+    char fname[FILENAME_MAX];
+    char buf1[256];
+    char buf2[256];
+    FILE* fp1;
+    FILE* fp2;
+
+    printf("input the filename of height:");
+    fgets(fname,sizeof(fname),stdin);
+    fname[strlen(fname)-1] = '\0';
+    printf("the filename of sample: %s\n",fname);
+
+    fp1 = fopen(fname,"r");
+    if(fp1==NULL){
+        fputs("File open error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("input the filename of IDs:");
+    fgets(fname,sizeof(fname),stdin);
+    fname[strlen(fname)-1] = '\0';
+    printf("the filename of sample: %s\n",fname);
+
+    fp2 = fopen(fname,"r");
+    if(fp2==NULL){
+        fputs("File open error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+    //============================================
+
+    struct student stData[14];
+    fgets(buf1,sizeof(buf1),fp1);//一行目の[gender, hight(cm)]を読み飛ばす
+
+    sp=stData;
+    while (1){
+        if(fgets(buf1,sizeof(buf1),fp1) ==NULL)//height読み込み,最後までロードしたら終了
+            break;
+        fgets(buf2,sizeof(buf2),fp2);   //IDs読み込み
+
+        sscanf(buf1,"%d, %lf",&gen,&height);
+        sscanf(buf2,"%d",&num);
+
+        (*sp).gen=gen;
+        (*sp).height=height;
+        (*sp).num=num;
+
+        sp++;
+    }
+
+    //検索numの入力
+    printf("which ID's data do you want?:");
+    scanf("%d",&imput);
+
+    for (sp=stData; sp!=NULL; sp++)
+    {
+        if ((*sp).num == imput)
+        {
+            printf("---\n");
+            printf("ID:%d\n",(*sp).num);
+            if((*sp).gen==1)
+                printf("gender:male\n");
+            else
+                printf("gender:female\n");
+            printf("height:%.3lf\n",(*sp).height);
+            printf("---\n");
+            return 0;
+        }
+    }
+    printf("no data\n");
+    return 0;
+
+}
